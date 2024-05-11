d8e3eab5270179d6d17548faf47fbb4bc12eca06
Developer <developer@mdgmyanmar.com>
2024-05-11 09:12:03 +0630

    [JL-3] POS Print Slip(Date Format)

--- custom_pos_receipt/static/src/js/Receipt.js
+++ custom_pos_receipt/static/src/js/Receipt.js
@@ -7,6 +7,17 @@ odoo.define('custom_pos_receipt.receipt', function(require){
             var self = this;
             models.load_fields('res.partner', ['barcode']);
             _super_partner.initialize.apply(this, arguments);
-        }
+        },
+        getValidationDate: function () {
+            var order = this.get_order();
+            if (order) {
+                return this._format_validation_date(order.validation_date);
+            }
+            return '';
+        },
+        _format_validation_date: function (date) {
+            if (!date) return '';
+            return moment(date).format('D-MMM-Y, hh:mm A'); // Adjust the format as needed
+        },
     });
 });
